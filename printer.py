08521b834bbb0e52594901ea3dab5df707b72b85
Jerome Cheng <jerome@ayulin.net>
2015-10-09 15:54:22 +0800

    printer module docstrings.

--- printer.py
+++ printer.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 
 
 def print_all(results_dir):
+    """Given the path to the results directory, writes results for each
+    network into a CSV file."""
     twitter_directory = os.path.join(results_dir, 'Twitter')
     twitter_results = load_results(twitter_directory)
     processed_twitter_results = process_results_for_printing(twitter_results)
@@ -25,6 +27,8 @@ def print_all(results_dir):
 
 
 def process_results_for_printing(results):
+    """Given a dictionary nested in the hierarchy of `system: class: results`,
+    returns one nested in a hierarchy of `class: system: results`."""
     # Results are loaded as (system:class:results) nested dictionaries.
     # It's easier to work with them in (class:system:results) form.
     official = {}
@@ -40,6 +44,9 @@ def process_results_for_printing(results):
 
 
 def print_network_results(results):
+    """Given a results dictionary, returns CSV lines for the performance
+    of each system, separated by class, then metric (F1, Precision, and
+    Recall)."""
     # Split by class, then by metric.
     output_lines = ['\n\nOfficial']
     output_lines += print_class_results(results['official'])
@@ -50,6 +57,8 @@ def print_network_results(results):
 
 
 def print_class_results(results):
+    """Given a results dictionary, returns CSV lines for each system's F1,
+    precision, and recall."""
     f1 = print_f1(results)
     precision = print_precision(results)
     recall = print_recall(results)
@@ -58,18 +67,27 @@ def print_class_results(results):
 
 
 def print_f1(results):
+    """Given a results dictionary (nested in `system: results` form), returns
+    CSV lines for each system's F1."""
     return print_metric(results, key='f1')
 
 
 def print_precision(results):
+    """Given a results dictionary (nested in `system: results` form), returns
+    CSV lines for each system's precision."""
     return print_metric(results, key='precision')
 
 
 def print_recall(results):
+    """Given a results dictionary (nested in `system: results` form), returns
+    CSV lines for each system's recall."""
     return print_metric(results, key='recall')
 
 
 def print_metric(results, key):
+    """Given a results dictionary (nested in `system: results` form)
+    and a metric (f1, precision, or recall), returns CSV lines for each
+    system's results in that metric."""
     output_lines = ['\n\n' + key + ' Averages']
 
     systems = sorted(results.iterkeys())
@@ -95,7 +113,7 @@ def print_metric(results, key):
 
 def load_results(directory):
     """Loads all result dictionaries in the given directory,
-    and returns them in a dictionary keyed by the system name."""
+    and returns them in a dictionary keyed by their system's name."""
     results = {}
     for filename in os.listdir(directory):
         path = os.path.join(directory, filename)
