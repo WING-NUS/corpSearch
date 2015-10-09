21a377d7324c2d54940472352c95d9c0f54a2a45
Jerome Cheng <jerome@ayulin.net>
2015-10-09 16:07:21 +0800

    SimpleDiskCache docstrings.

--- simplediskcache/SimpleDiskCache.py
+++ simplediskcache/SimpleDiskCache.py
@@ -9,6 +9,7 @@ from pymongo import DESCENDING
 import mongo
 
 def cache(filename):
+    """Decorator that implements a file-based cache for the given method."""
     def wrapper(func):
         @functools.wraps(func)
         def cacher(*args, **kwargs):
@@ -40,7 +41,10 @@ def cache(filename):
 
 
 def expiring_cache(filename, time_in_seconds):
-
+    """Decorator that implements a MongoDB-based expiring cache
+    for the given function. If the result stored in the DB is >
+    time_in_seconds old, the function is invoked, and the result
+    stored and returned."""
     def wrapper(func):
         @functools.wraps(func)
         def cacher(*args, **kwargs):
@@ -72,12 +76,16 @@ def expiring_cache(filename, time_in_seconds):
 
 
 def ensure_cache_dir_exists():
+    """Creates the cache directory if it does not already exist."""
     if not os.path.exists('cache'):
         os.mkdir('cache')
 
 
 @expiring_cache('fibonacci', 600)
 def fibonacci(n):
+    """Demo method for the expiring cache. Calculates
+    the n-th fibonacci number, with results expiring
+    after ten minutes."""
     if n == 0 or n == 1:
         return 1
 
