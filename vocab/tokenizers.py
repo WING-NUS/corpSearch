99d1d361cea8a29e6aee368a23351c3311b50025
Jerome Cheng <jerome@ayulin.net>
2015-10-10 16:07:58 +0800

    Docstrings and doctests for tokenizers.

--- vocab/tokenizers.py
+++ vocab/tokenizers.py
@@ -3,6 +3,11 @@ class BaseTokenizer(object):
     """The simplest tokenizer: splits strings by whitespace."""
     @staticmethod
     def tokenize(string):
+        """Tokenizes the given string.
+
+        >>> BaseTokenizer.tokenize('Hello world.')
+        ['Hello', 'world.']
+        """
         return string.split()
 
 
@@ -10,5 +15,10 @@ class LowerTokenizer(object):
     """Casts strings to lower-case before splitting by whitespace."""
     @staticmethod
     def tokenize(string):
+        """Tokenizes the given string.
+
+        >>> LowerTokenizer.tokenize('Hello world.')
+        ['hello', 'world.']
+        """
         lowered = string.lower()
         return BaseTokenizer.tokenize(lowered)
