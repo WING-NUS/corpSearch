6f94ed64220b7201ef485615e45b4bd8eb3b2b58
Jerome Cheng <jerome@ayulin.net>
2015-10-10 16:13:18 +0800

    Docstrings for vocab.test unit tests.

--- vocab/tests/test_tokenizers.py
+++ vocab/tests/test_tokenizers.py
@@ -3,6 +3,8 @@ from ..tokenizers import *
 
 class TestTokenizers:
     def test_base_string_properly_split(self):
+        """Tests that the BaseTokenizer correctly tokenizes a string by
+        whitespace."""
         sentence = "The quick brown fox jumped over the lazy dog."
         expected = ["The", "quick", "brown", "fox", "jumped", "over", "the",
                     "lazy", "dog."]
@@ -10,6 +12,8 @@ class TestTokenizers:
         assert BaseTokenizer.tokenize(sentence) == expected
 
     def test_string_properly_lowered(self):
+        """Tests that the LowerTokenizer correctly converts a string
+        to lower-case before tokenizing."""
         sentence = "The Quick Brown Fox Jumped Over The Lazy Dog."
         expected = ["the", "quick", "brown", "fox", "jumped", "over", "the",
                     "lazy", "dog."]
--- vocab/tests/test_vector_space.py
+++ vocab/tests/test_vector_space.py
@@ -2,6 +2,8 @@ from ..vectorspace import VectorSpaceSearch
 
 
 def test_identical_query():
+    """Tests that a query that is identical to a document returns a
+    cosine similarity of 1.0."""
     documents = [
         'The quick brown fox',
     ]
